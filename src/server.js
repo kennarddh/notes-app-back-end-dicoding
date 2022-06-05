4e9fb161a1513368ecec0ebac3243264560a104e
kennarddh <79390992+kennarddh@users.noreply.github.com>
2022-06-05 23:28:03 +0700

    feat: Membuat HTTP Server

--- src/server.js
+++ src/server.js
@@ -1,1 +1,15 @@
-console.log('Hallo kita akan membuat RESTful API')
+const Hapi = require('@hapi/hapi')
+
+const init = async () => {
+	const server = Hapi.server({
+		port: 5000,
+		host: 'localhost',
+	})
+
+	await server.start()
+
+	// eslint-disable-next-line no-console
+	console.log(`Server berjalan pada ${server.info.uri}`)
+}
+
+init()
