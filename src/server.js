86a5dace6dd08433cb0412fbc91b3f62f4e6ec46
kennarddh <79390992+kennarddh@users.noreply.github.com>
2022-06-06 11:45:32 +0700

    feat: Menyimpan Catatan

--- src/server.js
+++ src/server.js
@@ -1,4 +1,5 @@
 const Hapi = require('@hapi/hapi')
+const routes = require('./routes')
 
 const init = async () => {
 	const server = Hapi.server({
@@ -7,6 +8,8 @@ const init = async () => {
 	})
 
 	await server.start()
+
+	server.route(routes)
 
 	// eslint-disable-next-line no-console
 	console.log(`Server berjalan pada ${server.info.uri}`)
